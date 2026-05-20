46eb39ea0fd8c230cfc2f56de44fb53487ecc267
nguyen.trathao <nguyen.trathao@asnet.com.vn>
2026-05-20 15:46:37 +0700

    feat: refactor ESLint

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,14 +1,27 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+
+/** @type {import('eslint').Linter.Config[]} */
+const config = [
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
   },
-  rules: {
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-explicit-any': 'warn',
-  },
-};
+];
+
+module.exports = config;
+module.exports.config = config;
--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -1,7 +1,15 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  extends: ['./base.js', 'next/core-web-vitals'],
-  rules: {
-    '@next/next/no-html-link-for-pages': 'off',
+const coreWebVitals = require('eslint-config-next/core-web-vitals');
+
+/** @type {import('eslint').Linter.Config[]} */
+const config = [
+  ...(Array.isArray(coreWebVitals) ? coreWebVitals : [coreWebVitals]),
+  {
+    rules: {
+      '@next/next/no-html-link-for-pages': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
   },
-};
+];
+
+module.exports = config;
