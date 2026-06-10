00380e45711b2ca6a30183f9965a181e828a62b8
My Pham Ha Le <my.le@asnet.com.vn>
2026-06-10 17:41:35 +0700

    feat: update assistant message

--- apps/web/src/components/chat/CustomAssistantMessage/index.tsx
+++ apps/web/src/components/chat/CustomAssistantMessage/index.tsx
@@ -11,16 +11,16 @@ const CustomAssistantMessage = (props: AssistantMessageProps) => {
   const renderAfter = Boolean(assistantUi && assistantUiPosition !== 'before');
 
   return (
-    <div className="flex max-w-[50%] gap-3 py-2">
+    <div className="flex max-w-[80%] gap-3 py-2">
       <div className="text-brand-500 border-border-tertiary flex h-10 w-10 shrink-0 items-center justify-center rounded-full border-2">
         <Bot size={18} />
       </div>
-      <div className="flex-1">
+      <div>
         {renderBefore ? <div className="mb-2">{assistantUi}</div> : null}
 
         <div
-          className={`border-border-tertiary text-text-primary rounded-[28px] border p-4 ${
-            isCurrentMessage ? 'shadow-sm' : 'shadow-xs'
+          className={`border-border-tertiary text-text-primary card-shadow rounded-[28px] border p-4 ${
+            isCurrentMessage ? 'ring-border-secondary ring-1' : ''
           }`}
         >
           {content ? (
