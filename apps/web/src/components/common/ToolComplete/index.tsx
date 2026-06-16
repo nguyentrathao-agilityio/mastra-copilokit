6d15468e55cbfb006268a1487d8441fd092bda9a
My Pham Ha Le <my.le@asnet.com.vn>
2026-06-16 12:14:33 +0700

    feat: update tool status style

--- apps/web/src/components/common/ToolComplete/index.tsx
+++ apps/web/src/components/common/ToolComplete/index.tsx
@@ -7,7 +7,7 @@ interface ToolCompleteProps {
 
 export const ToolComplete = ({ action = 'running', target }: ToolCompleteProps) => {
   return (
-    <div className="bg-background-secondary text-text-primary mb-2 flex h-10 max-w-2xl items-center rounded-[28px] px-4 py-2 shadow">
+    <div className="bg-background-primary text-text-primary mb-2 flex h-10 max-w-2xl items-center rounded-[28px] px-4 py-2 shadow">
       <div className="flex items-center justify-center gap-2 text-sm">
         <CheckCircle2 className="text-badge-success-text h-4 w-4" />
         <span>
--- apps/web/src/components/common/ToolLoading/index.tsx
+++ apps/web/src/components/common/ToolLoading/index.tsx
@@ -7,7 +7,7 @@ interface ToolLoadingProps {
 
 export const ToolLoading = ({ action = 'Searching for', target }: ToolLoadingProps) => {
   return (
-    <div className="bg-background-secondary text-text-primary flex h-10 items-center rounded-[28px] px-4 py-2 shadow">
+    <div className="bg-background-primary text-text-primary flex h-10 items-center rounded-[28px] px-4 py-2 shadow">
       <div className="text-text-primary flex items-center gap-2 text-sm">
         <Loader2 className="h-4 w-4 animate-spin" />
         <span>
